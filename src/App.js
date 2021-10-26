0266052c351350ffcd6cb56992b1795f3aa73850
ermak-yana <lidl.peperts.stor@gmail.com>
2021-10-26 22:20:31 +0300

    fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div></div>;
-}
+export default class App extends Component {
+  state = {
+    images: null,
+  };
+  componentDidMount() {
+    fetch(
+      "https://pixabay.com/api/?q=banan&page=1&key=23265358-c42edc99dfdc4b7aade5bc218&image_type=photo&orientation=horizontal&per_page=12"
+    )
+      .then((res) => res.json())
+      .then((images) => this.setState({ images }));
+  }
 
-export default App;
+  render() {
+    return <div>{this.state.images && <div>{this.state.images.id}</div>}</div>;
+  }
+}
