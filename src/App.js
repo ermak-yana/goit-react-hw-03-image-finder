de0175b0b70d142cbc1177a1340fccd600992f2e
ermak-yana <lidl.peperts.stor@gmail.com>
2021-10-28 11:43:47 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,19 +1,119 @@
 import { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import getImages from "./servise/getImages.js";
+import Searchbar from "./components/Searchbar/Searchbar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import Button from "./components/Button/Button";
+import Modal from "./components/Modal/Modal";
 
-export default class App extends Component {
+class App extends Component {
   state = {
-    images: null,
+    value: "",
+    isLoading: false,
+    gallery: [],
+    page: 1,
+    isShow: false,
+    largeImageURL: "",
   };
-  componentDidMount() {
-    fetch(
-      "https://pixabay.com/api/?q=banan&page=1&key=23265358-c42edc99dfdc4b7aade5bc218&image_type=photo&orientation=horizontal&per_page=12"
-    )
-      .then((res) => res.json())
-      .then((images) => this.setState({ images }));
+
+  onChangeState = (value) => {
+    this.setState({ value: value, page: 1, gallery: [] });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.value !== this.state.value ||
+      prevState.page !== this.state.page
+    ) {
+      this.getApiImages();
+    }
   }
+
+  getApiImages = () => {
+    const { page, value } = this.state;
+    this.setState({ isLoading: true });
+
+    getImages(value, page)
+      .then((hits) => {
+        if (hits.length === 0) {
+          this.toastError();
+        }
+        this.setState((prevState) => ({
+          gallery: [...prevState.gallery, ...hits],
+        }));
+      })
+      .then(() => {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: "smooth",
+        });
+      })
+      .catch()
+      .finally(() => this.setState({ isLoading: false }));
+  };
+
+  onClickImgOpen = () => {
+    this.setState(({ isShow }) => ({
+      isShow: !isShow,
+    }));
+  };
+
+  loadMore = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  changeSrc = ({ largeImageURL }) => {
+    this.setState({ largeImageURL });
+  };
+
+  toastError = () => {
+    toast.error("Nothing found for your request", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
 
   render() {
-    return <div>{this.state.images && <div>{this.state.images.id}</div>}</div>;
+    const { gallery, isLoading, largeImageURL, isShow } = this.state;
+
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.onChangeState}></Searchbar>
+        <ImageGallery
+          gallery={gallery}
+          onClickImgOpen={this.onClickImgOpen}
+          changeSrc={this.changeSrc}
+        />
+        {isLoading && <Loader />}
+        {gallery.length > 0 ? <Button onClick={this.loadMore} /> : null}
+        {isShow && (
+          <Modal onClose={this.onClickImgOpen} largeImageURL={largeImageURL} />
+        )}
+
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div>
+    );
   }
 }
+
+export default App;
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,1 +1,28 @@
-<ul className="ImageGallery"></ul>;
+import PropTypes from "prop-types";
+import css from "../ImageGallery/ImageGallery.module.css";
+import CreateGaleryItem from "../ImageGalleryItem/ImageGalleryItem";
+
+function CreateImageList({ gallery, onClickImgOpen, changeSrc }) {
+  return (
+    <ul className={css.ImageGallery}>
+      {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
+        <CreateGaleryItem
+          onClickImgOpen={onClickImgOpen}
+          key={id}
+          tags={tags}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          changeSrc={changeSrc}
+        />
+      ))}
+    </ul>
+  );
+}
+
+CreateImageList.propTypes = {
+  gallery: PropTypes.array,
+  onClickImgOpen: PropTypes.func,
+  changeSrc: PropTypes.func,
+};
+
+export default CreateImageList;
